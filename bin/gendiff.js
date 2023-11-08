89be9ed8429e59d843bbd24022e0ecf2d459a59d
titonatos <titanium4ik777@gmail.com>
2023-11-08 13:32:24 +0300

    step 3 done

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,8 +1,19 @@
 #!/usr/bin/env node
 import { program } from 'commander';
+import getData from '../src/parse.js';
 
 program
   .version('0.1.0')
   .description('Compares two configuration files and shows a difference.');
+
+program
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .action((filepath1, filepath2) => {
+    // console.log(JSON.parse(readFile(filepath1)));
+    // console.log(JSON.parse(readFile(filepath2)));
+    console.log(getData('host', filepath1));
+  });
 
 program.parse(process.argv);
---
+++ src/parse.js
@@ -0,0 +1,21 @@
+import fs from 'node:fs';
+import process from 'node:process';
+import path from 'node:path';
+
+const readFile = (fileName) => {
+  const currenDirectory = process.cwd();
+  const absoluteFilePath = path.resolve(currenDirectory, fileName);
+
+  return fs.readFileSync(absoluteFilePath);
+};
+
+const getFileType = (fileName) => fileName.split('.').at(-1);
+
+export default (dataName, fileName) => {
+  const fileData = readFile(fileName);
+  const fileType = getFileType(fileName);
+
+  if (fileType.toLowerCase() === 'json') {
+    return JSON.parse(fileData)[dataName];
+  }
+};
