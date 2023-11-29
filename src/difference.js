abdcc32cb95c01bf9df1a099ec5c8776f16a86d2
titonatos <titanium4ik777@gmail.com>
2023-11-29 13:20:55 +0300

    refactored difference.js stylish.js

--- src/difference.js
+++ src/difference.js
@@ -2,41 +2,46 @@ import _ from 'lodash';
 import getData from './parsers.js';
 import format from './format.js';
 
+const getStatuses = {
+  noChange: 'noChange',
+  added: 'added',
+  removed: 'removed',
+};
+
 const getDifference = (path1, path2, formater = 'stylish') => {
   const file1Data = getData(path1);
   const file2Data = getData(path2);
 
   const iter = (file1, file2, depth = 1) => {
     const isObject = (obj) => Object.prototype.toString.call(obj) === '[object Object]';
+    // const isObject = (obj) => typeof obj === 'object' && obj !== null;
+
+    const { noChange, added, removed } = getStatuses;
 
     const keys1 = Object.keys(file1);
     const keys2 = Object.keys(file2);
 
-    const noChangeStatus = 'noChange';
-    const addedStatus = 'added';
-    const removedStatus = 'removed';
-
     const file1Difference = keys1.flatMap((key) => {
       if (keys2.includes(key)) {
         if (isObject(file1[key]) && isObject(file2[key])) {
-          return { key, value: iter(file1[key], file2[key], depth + 1), status: noChangeStatus };
+          return { key, value: iter(file1[key], file2[key], depth + 1), status: noChange };
         }
 
         if (file1[key] === file2[key]) {
-          return { key, value: file1[key], status: noChangeStatus };
+          return { key, value: file1[key], status: noChange };
         }
 
         return [
-          { key, value: file1[key], status: removedStatus },
-          { key, value: file2[key], status: addedStatus }];
+          { key, value: file1[key], status: removed },
+          { key, value: file2[key], status: added }];
       }
 
-      return { key, value: file1[key], status: removedStatus };
+      return { key, value: file1[key], status: removed };
     });
 
     const file2Additional = keys2
-      .filter((key) => keys1.includes(key) === false)
-      .flatMap((key) => ({ key, value: file2[key], status: addedStatus }));
+      .filter((key) => !keys1.includes(key))
+      .flatMap((key) => ({ key, value: file2[key], status: added }));
 
     return _.sortBy(
       _.concat(file1Difference, file2Additional),
--- src/stylish.js
+++ src/stylish.js
@@ -1,6 +1,5 @@
-/* eslint-disable max-len */
 const stylish = (data) => {
-  const status = {
+  const prefixes = {
     noChange: '  ',
     added: '+ ',
     removed: '- ',
@@ -9,56 +8,67 @@ const stylish = (data) => {
 
   const getIndent = (multipler, correction = 2, indent = ' ', indentPerMultipler = 4) => `${indent.repeat(multipler * indentPerMultipler - correction)}`;
 
-  const getLine = (depth, key, value, prefix, correction = 2) => `${getIndent(depth, correction) + prefix + key}: ${value}`;
-
   const isObject = (obj) => Object.prototype.toString.call(obj) === '[object Object]';
 
   const getKey = (object) => object.key;
 
   const getValue = (object, value = 'value') => object[value];
 
-  const hasArrayChildren = (obj) => Array.isArray(getValue(obj));
-
   const hasObjectChildren = (obj) => isObject(getValue(obj));
 
   const getStatus = (object) => object.status;
+
+  const formatLine = (key, value, status, depth, correction = 2) => `${getIndent(depth, correction) + prefixes[status] + key}: ${value}`;
+
+  const formatObject = (key, status, children, depth) => {
+    const formattedChildren = Array.isArray(children) ? children.join('\n') : children;
+
+    return [
+      `${getIndent(depth) + prefixes[status] + key}: {`,
+      formattedChildren,
+      `${getIndent(depth, 0)}}`,
+    ];
+  };
 
   const iter = (dataToIter, depth = 0) => {
     if (Array.isArray(dataToIter)) {
       return dataToIter.flatMap((obj) => iter(obj, depth + 1)).join('\n');
     }
 
-    if (hasArrayChildren(dataToIter)) {
-      return [
-        `${getIndent(depth) + status[getStatus(dataToIter)] + getKey(dataToIter)}: {`,
-        getValue(dataToIter).flatMap((obj) => iter(obj, depth + 1)).join('\n'),
-        `${getIndent(depth, 0)}}`,
-      ];
+    if (Array.isArray(getValue(dataToIter))) {
+      return formatObject(getKey(dataToIter), getStatus(dataToIter), getValue(dataToIter)
+        .flatMap((obj) => iter(obj, depth + 1)), depth);
     }
 
     if (hasObjectChildren(dataToIter)) {
-      return [
-        `${getIndent(depth) + status[getStatus(dataToIter)] + getKey(dataToIter)}: {`,
-        iter(getValue(dataToIter), depth + 1),
-        `${getIndent(depth, 0)}}`,
-      ].join('\n');
+      return formatObject(
+        getKey(dataToIter),
+        getStatus(dataToIter),
+        iter(
+          getValue(dataToIter),
+          depth + 1,
+        ),
+        depth,
+      );
     }
 
     if (getStatus(dataToIter) === undefined) {
-      return Object.keys(dataToIter).flatMap((key) => {
+      const formattedLines = Object.keys(dataToIter).map((key) => {
         if (isObject(dataToIter[key])) {
           return [
-            `${getIndent(depth, 0) + status[getStatus(dataToIter)] + key}: {`,
+            `${getIndent(depth, 0) + prefixes[getStatus(dataToIter)] + key}: {`,
             iter(dataToIter[key], depth + 1),
             `${getIndent(depth, 0)}}`,
           ].join('\n');
         }
 
-        return getLine(depth, key, dataToIter[key], status[getStatus(dataToIter)], 0);
-      }).join('\n');
+        return formatLine(key, dataToIter[key], getStatus(dataToIter), depth, 0);
+      });
+
+      return formattedLines;
     }
 
-    return getLine(depth, getKey(dataToIter), getValue(dataToIter), status[getStatus(dataToIter)]);
+    return formatLine(getKey(dataToIter), getValue(dataToIter), getStatus(dataToIter), depth);
   };
 
   return `{\n${iter(data)}\n}`;
